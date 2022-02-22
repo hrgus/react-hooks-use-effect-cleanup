201c8d6a6d14a665a1213f53fe71f4035adbf5e8
Harry <hrgus3@gmail.com>
2022-02-22 05:51:58 -0500

    Completed Assignment

--- src/Clock.js
+++ src/Clock.js
@@ -3,10 +3,16 @@ import React, { useState, useEffect } from "react";
 function Clock() {
   const [time, setTime] = useState(new Date());
 
+  console.log(Date());
+
   useEffect(() => {
-    setInterval(() => {
+    const timerID = setInterval(() => {
       setTime(new Date());
     }, 1000);
+
+    return function cleanup() {
+      clearInterval(timerID);
+    };
   }, []);
 
   return <div>{time.toString()}</div>;
